19428769a156a5e07df51e23b46e88a2ae46779e
Sergei Murashow <sergey.murashow@gmail.com>
2023-10-31 19:17:06 +0400

    .

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
 import express from 'express'
 
 import { bpiumRouter } from './routes/bpiumRouter'
-import { oneCRouter } from './routes/oneCRouter';
+import { oneCRouter } from './routes/oneCRouter'
 
-import GetConfig from './GetConfig';
+import GetConfig from './GetConfig'
 
 const { port } = GetConfig
 const configMessage = () => {
 	const arr = []
-	for( let i in GetConfig ) {
+	for (let i in GetConfig) {
 		arr.push(`${i}: ${GetConfig[i]}`)
 	}
 	return arr.join('\n')
 }
 
-const app = express();
+const app = express()
 app.use('/api/bpium', bpiumRouter)
 app.use('/api/1c', oneCRouter)
 
@@ -24,5 +24,4 @@ Running with params:
 ${configMessage()}
 `
 
-app.listen(port, () => console.log(startMessage));
-
+app.listen(port, () => console.log(startMessage))
